744b6955776ebbb3e7504dfd1301ab332bd609db
inspirsea <sunesson.emil@gmail.com>
2019-10-05 13:52:19 +0200

    player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private PlayerRenderer renderer;
+	private float moveSpeed = 500f;
+	private float maxMovespeed = 5f;
+	private Rigidbody2D body;
+
+	void Start()
+    {
+		renderer = GetComponentInChildren<PlayerRenderer>();
+		body = GetComponent<Rigidbody2D>();
+
+	}
+
+    void Update()
+    {
+		Turn(Input.GetAxis("Horizontal"));
+		Accelerate(Input.GetAxis("Vertical"));
+    }
+
+	private void Turn(float turn) {
+		var velocity = body.velocity;
+		var turnForce = (moveSpeed * turn);
+
+		body.AddForce(new Vector2(turnForce, velocity.y));
+
+		body.velocity = new Vector2(normalize(velocity.x, maxMovespeed), velocity.y);
+	}
+
+	private void Accelerate(float acceleration) {
+		var velocity = body.velocity;
+		var accelerationForce = (moveSpeed * acceleration);
+
+		body.AddForce(new Vector2(velocity.x, accelerationForce * 0.7f));
+
+		body.velocity = new Vector2(velocity.x, normalize(velocity.y, maxMovespeed));
+	}
+
+	private float normalize(float value, float maxValue) {
+		var result = value;
+
+		if (value > maxValue)
+		{
+			result = maxValue;
+		}
+		else if (value < -maxValue)
+		{
+			result = -maxValue;
+		}
+
+		return result;
+	}
+}
---
+++ Assets/Scripts/PlayerRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRenderer : MonoBehaviour
+{
+	public Sprite middle;
+	public Sprite left;
+	public Sprite right;
+
+	public void TurnLeft() {
+
+	}
+
+	public void TurnRight() {
+
+	}
+
+	public void Forward() {
+
+	}
+}
