c9d1e02057fa3001ee14f3f09b4ba0269d65c22d
Pontus Persson <eid.khorm@gmail.com>
2019-10-05 21:53:23 +0200

    Bullethell added

--- Assets/Scripts/Boss/Eye.cs
+++ Assets/Scripts/Boss/Eye.cs
@@ -8,17 +8,42 @@ public class Eye : MonoBehaviour
 	private bool dead = false;
 	private SpriteRenderer sprite;
 	private Collider2D coll;
+	private float spreadtime = 0.0f;
+	private HardPoint hp;
 
 	void Start()
     {
 		sprite = GetComponent<SpriteRenderer>();
 		coll = GetComponent<CircleCollider2D>();
+		hp = GetComponentInChildren<HardPoint>();
+	}
+
+	private void LateUpdate()
+	{
+		// want a angle between like 100 and 260?
+		var angle = 100 + (spreadtime * 32);
+		if (angle > 260)
+		{
+			spreadtime = 0.0f;
+			angle = 100;
+		}
+		hp.transform.rotation = Quaternion.Euler(0, 0, angle);
+
+		if (!sprite.enabled && hp)
+		{
+			hp.Fire();
+		}
+
+		//bigEye.GetComponentInChildren<HardPoint>().Fire();
+		spreadtime += Time.deltaTime;
 	}
 
 	public void ToggleEye()
 	{
+		if (dead) return;
 		sprite.enabled = !sprite.enabled;
 		coll.enabled = !coll.enabled;
+		spreadtime = 0.0f;
 	}
 
 	public void Kill()
