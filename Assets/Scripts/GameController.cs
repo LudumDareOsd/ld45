dc37bd422c58d3965925ab24ad7ec2905e95e3b4
Pontus Persson <eid.khorm@gmail.com>
2019-10-05 11:42:59 +0200

    Starting some basic scripts etc

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	private int score;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+	void Restart()
+	{
+		// TODO: Code to reset player restart map etc
+		score = 0;
+	}
+
+	public void AddScore()
+	{
+		score++;
+	}
+
+	public void GameOver()
+	{
+		Restart();
+	}
+
+}
