bd39b62014b61c8648e6d86b15a5cca425216cac
inspirsea <sunesson.emil@gmail.com>
2019-10-05 20:10:31 +0200

    bounds

---
+++ Assets/Scripts/Bounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds : MonoBehaviour
+{
+	private Vector2 screenBounds;
+
+	private void LateUpdate()
+	{
+		screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
+		var viewpos = transform.position;
+		viewpos.x = Mathf.Clamp(viewpos.x, (screenBounds.x * -1 + 1), screenBounds.x - 1);
+		viewpos.y = Mathf.Clamp(viewpos.y, (screenBounds.y * -1 + 1), screenBounds.y - 1);
+
+		transform.position = viewpos;
+
+	}
+}
