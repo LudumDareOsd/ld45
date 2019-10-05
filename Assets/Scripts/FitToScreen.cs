b75f375c59a89fd45e928b5727a1f0c50dee2073
inspirsea <sunesson.emil@gmail.com>
2019-10-05 11:09:29 +0200

    background

---
+++ Assets/Scripts/FitToScreen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitToScreen : MonoBehaviour
+{
+    void Update()
+    {
+		var height = Camera.main.orthographicSize * 2.0f;
+		var width = height * Screen.width / Screen.height;
+		transform.localScale = new Vector3(width, height, 0.1f);
+	}
+}
